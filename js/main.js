72ac47265267c731d5f3b9c96dade4a6469a3b72
CaleShanleyy <CaleShanley1@outlook.com>
2020-06-04 14:03:51 +1000

    Added modal

--- js/main.js
+++ js/main.js
@@ -35,10 +35,7 @@ $(document).ready(() => {
         board.push(false)
       }
     }
-    console.log(board)
   }
-
-  // reset board
 
   const checkWinner = function () {
     const combos = [
@@ -55,11 +52,55 @@ $(document).ready(() => {
     for (let i = 0; i < combos.length; i++) {
       const [a, b, c] = combos[i]
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        // Winning Row
-        console.log('winner', board[a], board[b], board[c])
+        // Confetti Effect for when you win
+        function fire(particleRatio, opts) {
+          setTimeout(function () {
+            confetti(
+              Object.assign({}, defaults, opts, {
+                particleCount: Math.floor(count * particleRatio)
+              })
+            )
+          }, 500)
+        }
+        fire(0.25, {
+          spread: 26,
+          startVelocity: 55
+        })
+        fire(0.2, {
+          spread: 60
+        })
+        fire(0.35, {
+          spread: 100,
+          decay: 0.91
+        })
+        fire(0.1, {
+          spread: 120,
+          startVelocity: 25,
+          decay: 0.92
+        })
+        fire(0.1, {
+          spread: 120,
+          startVelocity: 45
+        })
+        const modal = setTimeout(function () {
+          $.sweetModal({
+            content: `WINNER WINNER CHICKEN DINNER!`,
+            icon: $.sweetModal.ICON_SUCCESS
+          })
+        }, 1500)
       } else {
         // console.log(board);
       }
+    }
+  }
+
+  const declareWinner = function (winner) {
+    if (player1 === counter % 2) {
+      winner = 'player 1'
+      playerOnescore++
+    } else {
+      winner = 'player 2'
+      playerTwoscore++
     }
   }
 
@@ -72,5 +113,9 @@ $(document).ready(() => {
 })
 
 // Going to add a Confetti plugin for jQuery when you win
+const count = 200
+const defaults = {
+  origin: { y: 0.7 }
+}
 
 // Also need to reset
